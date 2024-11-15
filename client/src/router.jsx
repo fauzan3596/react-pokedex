96751aea081a07cba964593ee314a9c3b5c2038c
fauzan3596 <fauzan3596@gmail.com>
2024-11-15 13:30:06 +0700

    Inisialiasi vite project

---
+++ client/src/router.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                elemmet: <Home />
+            },
+            {
+                path: '/pokedex',
+                elemmet: <Home />
+            },
+            {
+                path: '/detail:id',
+                elemmet: <Home />
+            },
+            {
+                path: '/search',
+                elemmet: <Home />
+            },
+            {
+                path: '/favorite',
+                elemmet: <Home />
+            }
+        ]
+    }
+])
