e373ee5f7465a7b0ec7069166422415f9c20053d
Rachma Fauziah <rachma@Rachmas-MacBook-Air.local>
2024-11-19 02:51:58 +0700

    update card style

--- client/src/components/CardPokemon.jsx
+++ client/src/components/CardPokemon.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getPokemonDetails } from "../axios/fetchApi";
-import { Image, Col, Card } from 'react-bootstrap'
+import { Image, Row, Col, Card } from 'react-bootstrap'
 
 function CardPokemon({ url }) {
   const [detail, setDetail] = useState({});
@@ -16,18 +16,21 @@ function CardPokemon({ url }) {
         <Card className="w-100 card-pokemon" style={{ width: '18rem' }}>
           <Card.Img variant="top" src={detail.sprites?.other.home.front_default} />
           <Card.Body>
-            <Card.Text>#{detail.id}</Card.Text>
-            <Card.Title><b>{detail.name}</b></Card.Title>
-            <div>
-            {detail.types?.map((tipe, index) => {
-              return (
-                <>
-                <Image src={`/types/icons/${tipe.type.name}.png`} ></Image>
-                {tipe.type.name}
-                </>
-              )
-            })}
-            </div>
+            <Card.Text  className="d-flex justify-content-between align-items-start">
+              <div>
+                #{detail.id}
+                <h3><b>{detail.name}</b></h3>
+              </div>
+              <div>
+              {detail.types?.map((tipe, index) => {
+                return (
+                  <>
+                    <Image width="50px" src={`/types/icons/${tipe.type.name}.png`} ></Image>
+                  </>
+                )
+              })}
+              </div>
+            </Card.Text>
           </Card.Body>
         </Card>
       </Col>
