e1cae18d6d002de158b404a54c7a8e6d39f2f5c5
Rachma Fauziah <rachma@Rachmas-MacBook-Air.local>
2024-11-19 03:11:10 +0700

    change img

--- client/src/components/CardPokemon.jsx
+++ client/src/components/CardPokemon.jsx
@@ -16,16 +16,16 @@ function CardPokemon({ url }) {
         <Card className="w-100 card-pokemon" style={{ width: '18rem' }}>
           <Card.Img variant="top" src={detail.sprites?.other.home.front_default} />
           <Card.Body>
+            #{detail.id}
             <Card.Text  className="d-flex justify-content-between align-items-start">
               <div>
-                #{detail.id}
                 <h3><b>{detail.name}</b></h3>
               </div>
               <div>
               {detail.types?.map((tipe, index) => {
                 return (
                   <>
-                    <Image width="50px" src={`/types/icons/${tipe.type.name}.png`} ></Image>
+                    <Image width="25px" src={`/types/icons/${tipe.type.name}.svg`} />
                   </>
                 )
               })}
